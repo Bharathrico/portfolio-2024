6aa329fdc7c800a474445c2d7881058be332b1ef
bharathrico <bharathganesh26899@gmail.com>
2025-01-04 21:29:34 +0530

    additions

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Contacts from './Sections/Contacts';
 import Hero from './Sections/Hero';
 import Projects from './Sections/Projects';
 import Stack from './Sections/Stack';
+import Selector from './Sections/Selector.js';
 function App() {
   return (
     <div className="App">
+    <Selector/>
      <Hero/>
      <Projects/>
      <Stack/>
---
+++ src/Sections/Selector.js
@@ -0,0 +1,12 @@
+import './Selector.css'
+
+function Selector() {
+    return (  <div className="Selector">
+            <span>Home</span>
+            <span>Home</span>
+            <span>Home</span>
+            <span>Home</span>
+        </div>);
+}
+
+export default Selector;
